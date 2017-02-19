094a6a3ac15a21ef4fda6ef3280b377ec6ace7d6
Riza Fahmi <rizafahmi@gmail.com>
2017-02-19 07:13:29 +0700

    Router middleware protection with token verify

--- server.js
+++ server.js
@@ -75,6 +75,29 @@ apiRoutes.post('/authenticate', (req, res) => {
   })
 })
 
+apiRoutes.use((req, res, next) => {
+  const token = req.body.token || req.query.token || req.headers['x-access-token']
+
+  if (token) {
+    jwt.verify(token, app.get('superSecret'), (err, decoded) => {
+      if (err) {
+        return res.json({
+          status: 'OK',
+          message: 'Failed to authenticate token.'
+        })
+      } else {
+        req.decoded = decoded
+        next()
+      }
+    })
+  } else {
+    return res.status(403).send({
+      status: 'OK',
+      message: 'No token provided.'
+    })
+  }
+})
+
 apiRoutes.get('/', (req, res) => {
   res.json({
     message: 'Welcome to the coolest API on earth!',
